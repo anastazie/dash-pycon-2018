0f08a393851f76cf63f0c5b3bdd15835c152fb80
Anastazie Sedlakova <nasta@sedlakovi.com>
2018-05-26 11:28:25 +0200

    add project to work with

---
+++ example_project.py
@@ -0,0 +1,53 @@
+import dash.dependencies
+import dash_html_components as html
+import dash_core_components as dcc
+import os
+import pandas as pd
+import plotly.graph_objs as go
+
+
+app = dash.Dash()
+
+app.layout = html.Div(children=[
+
+	html.Div([
+		html.Label('Hello, What do you like to do in your free time')],
+		style={
+			'display': 'inline-block', 'vertical-align': 'middle',
+			'textAlign': 'center', 'font-size': '1.6em', 'width': '40%'
+	}),
+
+	dcc.Dropdown(
+	 id='example-dropdown',
+		options=[
+			{'label': 'Read books', 'value': 'read'},
+			{'label': 'Bake cakes', 'value': 'bake'},
+		],
+		value=''
+	),
+
+    dcc.Graph(
+        id='example-gplot',
+        figure={
+            'data': [
+            	go.Bar(x=[1], y=[628], name='Paperback'),
+                go.Bar(x=[1], y=[796], name='Hard book')
+            ],
+            'layout': {
+                'title': 'Book weight in grams'
+                }
+            }
+    )
+])
+
+@app.callback(
+    dash.dependencies.Output(component_id='example-plot', component_property='figure'),
+    [dash.dependencies.Input(component_id='example-dropdown', component_property='value')]
+)
+
+def update_plot(choice):
+	data=[]
+	return data
+
+if __name__ == '__main__':
+	app.run_server()
